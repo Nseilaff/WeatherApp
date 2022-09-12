06ae2f101a16b53d8b7d860e61e92a41878dc175
Nate Seilaff <seilaffn@gmail.com>
2022-09-12 08:12:43 -0600

    Add project files.

---
+++ WeatherApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Linq;
+
+var client = new HttpClient(); //http request
+Console.WriteLine("Please Enter your Zip Code");
+var zipCode = Console.ReadLine();
+var APIkey = ;
+var weather = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={APIkey}";
+
+var weatherResponse = client.GetAsync(weather).Result;
+
+var getWeather = JArray.Parse(weatherResponse.Content.ReadAsStringAsync().Result);
+
